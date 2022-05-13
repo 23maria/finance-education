06e4b11ee23421d66032df5a33e29a189ca60c65
23maria <mariart2023@gmail.com>
2022-05-14 01:29:39 +0530

    wed

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ var i = 2;
 function switchTAB() {
 	var x = document.getElementById("list-switch");
 	if(i%2 == 0) {
-		document.getElementById("list-switch").style= "display: grid; height: 50vh; margin-left: 5%;";
-		document.getElementById("search-switch").style= "display: block; margin-left: 5%;";
+		document.getElementById("list-switch").style= "display: grid; height: 50vh; margin-left: 2%;";
+		document.getElementById("search-switch").style= "display: block; margin-left: 1%;";
 	}else {
 		document.getElementById("list-switch").style= "display: none;";
 		document.getElementById("search-switch").style= "display: none;";
